083bff6b76e19088cb3b4670e23310e0d9c53af4
romeyiow <77595388+romeyiow@users.noreply.github.com>
2024-05-17 23:45:40 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,45 @@
+<?php
+// SUB-TASKS NO.1 & 2
+$name = "Jerome Imperial";
+$age = 24;
+$address = "Mabini Street,Poblacion, Pamplona, Camarines Sur";
+// $hobbies = "Playing Chess,Karaoke,Intellectual Entertainment";
+// $pet_peeve = "Hindi nagsasampay ng tuwalya pagkatapos gumamit.";
+echo $name . "<br>";
+echo $age . "<br>";
+echo $address . "<br>";
+echo $hobbies . "<br>";
+echo $pet_peeve . "<br>";
+
+
+// SUB-TASKS NO.3
+define("NAME", "Jerome Imperial");
+define("AGE", 24);
+define("ADDRESS", "Mabini Street,Poblacion, Pamplona, Camarines Sur");
+define("HOBBIES", "Playing Chess,Karaoke,Intellectual Entertainment");
+define("PET_PEEVE", "Hindi nagsasampay ng tuwalya pagkatapos gumamit.");
+echo NAME . "<br>";
+echo AGE . "<br>";
+echo ADDRESS . "<br>";
+echo HOBBIES . "<br>";
+echo PET_PEEVE . "<br>";
+
+// SUB-TASKS NO.4
+echo "<pre>";
+var_dump(NAME);
+var_dump(AGE);
+var_dump(ADDRESS);
+var_dump(HOBBIES);
+var_dump(PET_PEEVE);
+echo "</pre>";
+
+// SUB-TASKS NO.5
+/* 
+Pag hindi po sinampay agad yung tuwalya 
+pagkatapos gamitin, mabilis
+pong bumabaho. nakakainis lang pag ikaw na 
+ang gagamit, mabaho na .-.
+lalabhan nanaman kaagad.
+mahirap maglaba lalo na po pag makapal
+yung tuwalya hahahaha 
+*/
